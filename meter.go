4e4e7374b384fe38b92aea8b1103e1cc74cf5ea5
Steven Allen <steven@stebalien.com>
2017-12-10 16:50:37 -0800

    add ForEach and Remove for MeterRegistry
    This way, we can find and remove, e.g., old meters.

--- meter.go
+++ meter.go
@@ -53,3 +53,16 @@ func (r *MeterRegistry) GetMeter(name string) *Meter {
 	m, _ := r.meters.LoadOrStore(name, new(Meter))
 	return m.(*Meter)
 }
+
+// RemoveMeter removes the named meter from the registry.
+func (r *MeterRegistry) RemoveMeter(name string) {
+	r.meters.Delete(name)
+}
+
+// ForEach calls the passed function for each registered meter.
+func (r *MeterRegistry) ForEach(iterFunc func(string, *Meter)) {
+	r.meters.Range(func(k, v interface{}) bool {
+		iterFunc(k.(string), v.(*Meter))
+		return true
+	})
+}
